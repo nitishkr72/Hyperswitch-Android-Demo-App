18500ed7e2da53733862f61a3d37c6c4ba3fe7e7
nitish-kr <nitishkr72@gmail.com>
2023-07-10 13:45:40 +0530

    Stripe Migration Using Script

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require("stripe")('sk_test_tR3PYbcVNZZ796tH88S4VQ2u');
+const stripe = require("stripe")("sk_test_tR3PYbcVNZZ796tH88S4VQ2u");
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -16,20 +16,80 @@ const calculateOrderAmount = (items) => {
 };
 
 app.post("/create-payment-intent", async (req, res) => {
-  const { items } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: "USD",
+      amount: 250000,
+      confirm: false,
+      capture_method: "automatic",
+      authentication_type: "no_three_ds",
+      customer_id: "StripeCustomer",
+      description: "Hello this is description",
+      shipping: {
+        address: {
+          line1: "1467",
+          line2: "Harrison Street",
+          line3: "Harrison Street",
+          city: "San Fransico",
+          state: "California",
+          zip: "94122",
+          country: "US",
+          first_name: "joseph",
+          last_name: "Doe",
+        },
+        phone: {
+          number: "8056594427",
+          country_code: "+91",
+        },
+      },
+      billing: {
+        address: {
+          line1: "1467",
+          line2: "Harrison Street",
+          line3: "Harrison Street",
+          city: "San Fransico",
+          state: "California",
+          zip: "94122",
+          country: "US",
+          first_name: "joseph",
+          last_name: "Doe",
+        },
+        phone: {
+          number: "8056594427",
+          country_code: "+91",
+        },
+      },
+      metadata: {
+        order_details: {
+          product_name: "Apple iphone 15",
+          quantity: 1,
+        },
+      },
+    });
 
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
-    currency: "usd",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
+    // Send PaymentIntent details to client
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (err) {
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "USD",
+        amount: 369999,
+      });
 
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  });
+      // Send publishable key and PaymentIntent details to client
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (err) {
+      return res.status(400).send({
+        error: {
+          message: err.message,
+        },
+      });
+    }
+  }
 });
 
 app.listen(4242, () => console.log("Node server listening on port 4242!"));
