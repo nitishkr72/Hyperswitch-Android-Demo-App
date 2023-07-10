1a28034ce4aa488a0b6503995808ec7716616a15
nitish-kr <nitishkr72@gmail.com>
2023-07-10 09:08:16 +0530

    .gitignore updated

---
+++ app/src/main/java/com/nitishkr/hyperswitchdemoapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.nitishkr.hyperswitchdemoapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+import com.stripe.android.PaymentConfiguration
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        PaymentConfiguration.init(
+            applicationContext,
+            "pk_test_51BTUDGJAJfZb9HEBwDg86TN1KNprHjkfipXmEDMb0gSCassK5T3ZfxsAbcgKVmAIXF7oZ6ItlZZbXO6idTHE67IM007EwQ4uN3"
+        )
+
+        findViewById<Button>(R.id.checkout).setOnClickListener {
+            val intent = Intent(this, CheckoutActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
